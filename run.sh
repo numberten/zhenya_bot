ea5d8faa3f53205bd3073556a4038251165152e1
Will Coster <willcoster@gmail.com>
2013-07-12 18:04:19 -0700

    Adding run.sh
    Depending on the exit code, this script will either:
    	0 	=> Quit
    	100 	=> Recompile and restart the bot
    	101 	=> Perform a git pull and then recompile and restart the bot

---
+++ run.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# Handles automatically restarting and updating zhenya_bot
+
+# Make sure that we are in the zhenya_bot source directory. It should be the
+# location of the script.
+DIR=`dirname $0`
+cd $DIR
+
+# Run the bot and save the exit status. The exit status determines if the bot is
+# restarted, updated or simply terminated.
+ghc bot.hs && ./bot
+status=`echo -n $?`
+echo $status
+if [ "$status" == 0 ]; then
+    echo "Stopping..."
+    exit 0
+elif [ "$status" == 100 ]; then
+    echo "Restarting..."
+    $DIR/run.sh
+elif [ "$status" == 101 ]; then
+    echo "Updating..."
+    git pull
+    $DIR/run.sh
+# There were likely syntax errors. In this case, simply exit.
+else
+    exit $status
+fi
