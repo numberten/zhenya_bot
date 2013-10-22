8f11d14caafe9074624696e7b242b2ece9c606cc
jesse <number.dot.ten@gmail.com>
2013-10-22 14:37:34 -0700

    Added support for shortened youtube urls in Youtube component

--- Bot/Component/Impl/Youtube.hs
+++ Bot/Component/Impl/Youtube.hs
@@ -7,16 +7,20 @@ import Bot.Component.Regex
 import Bot.IO
 
 import Control.Monad
+import Data.List (isPrefixOf)
 import Network.Curl.Download
 import Text.HTML.TagSoup
 
 youtube :: Bot Component
 youtube = regex pattern linkHandler
     where
-        pattern         = "youtube.com/watch\\?([a-zA-Z]+=[a-zA-Z0-9_#-]+&)*v=[a-zA-Z0-9_#-]+"
+        -- Matches youtube and shortened youtube urls.
+        pattern         = "youtube.com/watch\\?([a-zA-Z]+=[a-zA-Z0-9_#-]+&)*v=[a-zA-Z0-9_#-]+|youtu.be/[a-zA-Z0-9_#-]+"
 
         -- Run for each link that looks like a youtube video
-        linkHandler url =   liftIO (openAsTags ("http://www." ++ url))
+        linkHandler url =   (if "youtu." `isPrefixOf` url
+                            then liftIO (openAsTags ("http://www.youtube.com/watch?v=" ++ drop 1 (dropWhile (/= '/') url) ++ "&feature=youtu.be"))
+                            else liftIO (openAsTags ("http://www." ++ url)))
                         >>= either (const $ return ()) describeLink
 
         -- Attempt to pull the title of the video and relay it to IRC
