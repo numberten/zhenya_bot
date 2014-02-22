83bf8137416972af908e74b3d34f7ff9f8c535af
Will Coster <willcoster@gmail.com>
2014-02-22 00:38:12 -0800

    History service now saves logs (closes #8).
    Does not include time stamp. It would be nice if the format it saved in
    conformed to irssi or another standard format of logs so that it would work with
    pisg.

--- Bot/Component/Impl/History.hs
+++ Bot/Component/Impl/History.hs
@@ -9,8 +9,13 @@ module Bot.Component.Impl.History (
 import Bot.Component
 import Bot.IO
 
+import Control.Applicative
 import Control.Concurrent
 import Control.Monad.State
+import Prelude hiding (readFile)
+import System.FilePath
+import System.IO hiding (readFile)
+import System.IO.Strict
 import qualified Data.Sequence as S
 
 type Nick     = String
@@ -26,14 +31,34 @@ type HistoryHandle = MVar HistoryInfo
 -- | Creates a new ClusterNickHandle for use with the clusterNickService
 -- component. A reference to this required in order to make API calls.
 newHistoryHandle :: IO HistoryHandle
-newHistoryHandle = liftIO $ newMVar S.empty
+newHistoryHandle = liftIO newEmptyMVar
 
 -- | The `BotComponent` portion of the history service. This service must be
 -- included in the Bot otherwise all API calls will hang.
 historyService :: HistoryHandle -> Bot Component
-historyService handle = mkComponent $ onPrivMsg $ \message -> do
-    nick <- gets currentNick
-    liftIO $ modifyMVar_ handle  (return . (S.|> (nick, message)))
+historyService handle =  loadLog >> mkComponent action
+    where
+        action = onPrivMsg $ \message -> do
+            nick    <- gets currentNick
+            logFile <- logPath
+            let logLine = nick ++ '\t' : message
+            liftIO $ do
+                modifyMVar_ handle  (return . (S.|> (nick, message)))
+                withFile logFile AppendMode (`hPutStrLn` logLine)
+
+        logPath = do
+            directory <- gets dataDirectory
+            return (directory </> "log.txt")
+
+        loadLog = do
+            logFile <- logPath
+            liftIO $ do
+                log     <- map splitLine . lines <$> readFile logFile
+                putMVar handle (S.fromList log)
+
+        splitLine =   (,)
+                  <$> takeWhile (/= '\t')
+                  <*> drop 1 . dropWhile (/= '\t')
 
 getHistory  :: (BotMonad m, MonadIO m)
             => HistoryHandle -> m (S.Seq (Nick, Message))
