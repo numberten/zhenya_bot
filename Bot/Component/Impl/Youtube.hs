3462342c1845630305932c119dcb725a4502907b
jesse <number.dot.ten@gmail.com>
2014-02-28 13:02:13 -0800

    added video length to youtube component. modified youtube component to use mobile site.

--- Bot/Component/Impl/Youtube.hs
+++ Bot/Component/Impl/Youtube.hs
@@ -8,26 +8,54 @@ import Bot.IO
 
 import Control.Monad
 import Data.List (isPrefixOf)
-import Network.Curl.Download
+import Data.Maybe (isJust)
+import Network.Curl
+import Network.Curl.Opts
 import Text.HTML.TagSoup
+import Text.Regex
 
 youtube :: Bot Component
 youtube = regex pattern linkHandler
     where
-        -- Matches youtube and shortened youtube urls.
+        -- | Matches youtube and shortened youtube urls.
         pattern         = "youtube.com/watch\\?([a-zA-Z]+=[a-zA-Z0-9_#-]+&)*v=[a-zA-Z0-9_#-]+|youtu.be/[a-zA-Z0-9_#-]+"
 
-        -- Run for each link that looks like a youtube video
         linkHandler url =   (if "youtu." `isPrefixOf` url
-                            then liftIO (openAsTags ("http://www.youtube.com/watch?v=" ++ drop 1 (dropWhile (/= '/') url) ++ "&feature=youtu.be"))
-                            else liftIO (openAsTags ("http://www." ++ url)))
-                        >>= either (const $ return ()) describeLink
+                            then liftIO (scrape ("http://m.youtube.com/watch?v=" ++ drop 1 (dropWhile (/= '/') url) ++ "&feature=youtu.be"))
+                            else liftIO (scrape ("http://m." ++ url)))
+                        >>= ircReply 
 
-        -- Attempt to pull the title of the video and relay it to IRC
-        describeLink    =   mapM_ (ircReply . soupToTitle)
-                        .   sections (~== "<span id=\"eow-title\" title=\"\">")
+-- | Helper function that scrapes mobile youtube
+-- and returns the title and length of the url
+-- passed to it.
+scrape url = do
+    let useragent   =   CurlUserAgent "NOKIA"
+    (_,html)        <-  curlGetString url
+                    $   [useragent]
+    let tags        =   parseTags html
+    let title       =   tagToTitle
+                    .   head
+                    .   tail
+                    .   dropWhile (not . isTagOpenName "title")
+                    $   tags
+    let time        =   tagToTime
+                    .   head
+                    .   filter (isJust . match . fromTagText)
+                    .   filter isTagText
+                    $   tags
+    return          $   title ++ time
+    where
+        -- | Turns a TagText containing a time, into a properly
+        -- formatted string.
+        tagToTime  tag  =   '(' : init (tail (fromTagText tag)) ++ ")"
+        -- | Turns a TagText containing a title, into a properly
+        -- formatted string.
+        tagToTitle tag  =   reverse
+                        .   tail
+                        .   dropWhile (/='-')
+                        .   reverse
+                        $   fromTagText tag
+        -- | Regex for matching a video duration.
+        match           =   matchRegex 
+                        $   mkRegex "^\n[0-9]+:[0-9]+\n$"
 
-        -- Extract the title text from a bunch of tags
-        soupToTitle     =   concat
-                        .   liftM (fromAttrib "title")
-                        .   take 1
