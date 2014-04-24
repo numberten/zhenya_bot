f9825deb2b3c4629a9a8ee92bad2ca1224ef9e5a
keneanung <keneanung@googlemail.com>
2014-04-24 20:27:41 +0200

    Updated and optimized the n queens problem.
    I finally understood how foldM on lists work :happy: Since this provides an
    efficient way of backtracking, no guard is needed for reasonable numbers
    anymore.

--- Bot/Component/Impl/Queens.hs
+++ Bot/Component/Impl/Queens.hs
@@ -11,23 +11,24 @@ import Data.Char
 import Data.List
 
 calcQueens :: Bot Component
-calcQueens = command "!queens" setQueens 
+calcQueens = command "!queens" setQueens
     where
         setQueens = (ircReplyMaybe =<<) . queen . takeWhile isDigit . unwords
 
 queen :: String -> Bot ( Maybe String )
-queen a 
-      | n > 14 = return $ Just "I better not shoot myself by answering that!"
-      | otherwise = liftM ( Just . show )
-         $ return $ head [x | x <- permutations [1..n], safe x]
+queen a = liftM ( Just . show ) $ return $ head $ foldM foldingFunction [] [1..n]
    where
+      -- Our folding function. It works like this:
+      -- list: a list of already known safe positions from the previous fold
+      -- _: our input. But we don't need it as the input is only there to restrict the number of elements in the result lists
+      -- The function itself returns a list of lists, with the inner lists each having a position prepended that is safe
+      foldingFunction list _ = [x:list | x <- [1..n] \\ list, safe (x:list)]
       n = read a :: Int
-  
+
 safe :: [Int] -> Bool
 safe [] = True
-safe (x:xs) = and (map (\f -> safeDiag (f) (f x 1) xs) [ (+), (-) ]) && safe
-  xs
-   
+safe (x:xs) = and (map (\f -> safeDiag (f) (f x 1) xs) [ (+), (-) ])
+
 safeDiag :: ( Int -> Int -> Int ) -> Int -> [Int] -> Bool
 safeDiag _ _ [] = True
 safeDiag f field (x:xs) = field /= x && safeDiag f y xs
