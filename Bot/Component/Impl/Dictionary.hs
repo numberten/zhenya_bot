5110deb41452a5d6cde1aa7220bbab55eaa430d7
Will Coster <willcoster@gmail.com>
2014-05-03 21:54:44 -0700

    Substitute randomly defined word with search term.
    Closes: #16

--- Bot/Component/Impl/Dictionary.hs
+++ Bot/Component/Impl/Dictionary.hs
@@ -6,8 +6,13 @@ import Bot.Component
 import Bot.Component.Command
 import Bot.IO
 
+import Data.Char
+import Data.List
 import Network.HTTP
 import Text.HTML.TagSoup
+
+lookupPageUrl = "http://www.urbandictionary.com/define.php?term="
+randomPageUrl = "http://www.urbandictionary.com/random.php"
 
 define :: Bot Component
 define = command "!define" defineAction
@@ -18,13 +23,14 @@ define = command "!define" defineAction
                         .   fmap parseTags
                         $   getResponseBody
                         =<< simpleHTTP (getRequest
-                        $   "http://www.urbandictionary.com/define.php?term="
+                        $   lookupPageUrl
                         ++  map (\x -> if x == ' ' then '+' else x)
                                 (unwords words))
+
             redirect    <-  liftIO
                         .   fmap parseTags
                         $   getResponseBody
-                        =<< simpleHTTP (getRequest "http://www.urbandictionary.com/random.php")
+                        =<< simpleHTTP (getRequest randomPageUrl)
 
             let randomUrl   = fromAttrib "href" . head $ drop 3 redirect
 
@@ -33,22 +39,42 @@ define = command "!define" defineAction
                         $   getResponseBody
                         =<< simpleHTTP (getRequest randomUrl)
 
-            let isntNewline = \x -> if x == '\n' then False else True
-
-            let queriedDef  =   drop 1 
-                            .   dropWhile (~/= "<div class=\"meaning\">") 
+            let queriedDef  =   drop 1
+                            .   dropWhile (~/= "<div class=\"meaning\">")
                             $   tags
 
-            let randDef     =   drop 1 
-                            .   dropWhile (~/= "<div class=\"meaning\">") 
+            let targetWord  =   unwords words
+
+            let randWord    =   fromTagText
+                            .   head
+                            .   drop 3  -- skip the div and a tags
+                            .   dropWhile (~/= "<div class=\"word\">")
                             $   randomtags
 
-            let def = if (queriedDef == []) then randDef else queriedDef
+            let randDef     =   drop 1
+                            .   dropWhile (~/= "<div class=\"meaning\">")
+                            $   randomtags
 
-            let definition  =   filter isntNewline
+            let (def, rand) =   if (queriedDef == [])
+                                    then (randDef, True)
+                                    else (queriedDef, False)
+
+            let replaceWord =   replace randWord targetWord
+
+            let definition  =   (if rand then replaceWord else id)
+                            .   filter (/= '\n')
                             .   fromTagText
                             .   head
                             $   def
 
-            ircReply        $   unwords words ++ ": " ++ definition
+            ircReply        $   targetWord ++ ": " ++ definition
 
+        -- Performs a case insensitive replacement.
+        replace :: String -> String -> String -> String
+        replace _   _   []      = []
+
+        replace old new source
+            | isPrefixOf (map toLower old) (map toLower source)
+            = new ++ replace old new (drop (length old) source)
+
+        replace old new (x:xs)  = x : replace old new xs
