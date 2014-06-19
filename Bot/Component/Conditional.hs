6f1f2941c81618216d38c6eea96cc7bb8a5e4d2d
jesse <number.dot.ten@gmail.com>
2014-06-18 23:27:02 -0700

    changed Conditional components to read in match string. Added constConditional constructors for actions that take no arguments.

--- Bot/Component/Conditional.hs
+++ Bot/Component/Conditional.hs
@@ -1,6 +1,8 @@
 module Bot.Component.Conditional (
     conditional
 ,   conditionalT
+,   constConditional
+,   constConditionalT
 )   where
 
 import Bot.Component
@@ -11,12 +13,12 @@ import Control.Monad.Trans
 import Control.Monad.Trans.Identity
 
 -- | Similar to conditionalT but less generic and easier to use.
-conditional  :: (String -> Bool) -> Bot () -> Bot Component
+conditional  :: (String -> Bool) -> (String -> Bot ()) -> Bot Component
 conditional predicate action =
     mkComponentT $ conditionalT predicate actionT
     where
-        actionT :: IdentityT Bot ()
-        actionT = lift action
+        actionT :: String -> IdentityT Bot ()
+        actionT = lift . action
 
 -- | A generic conditional component that will execute an action when a given
 -- predicate over a PRIVMSG message is satisfied.
@@ -25,8 +27,27 @@ conditionalT ::  BotMonad b
             -- allowed to run.
             =>  (String -> Bool)
             -- | The action to be executed in the event of a match
+            ->  (String -> b ())
+            -- | Resulting Botable method
+            ->  String -> b ()
+conditionalT predicate action  =  onPrivMsgT $ \s -> when (predicate s) (action s)
+
+-- | Like conditional except the action performed takes no arguments.
+constConditional :: (String -> Bool) -> Bot () -> Bot Component
+constConditional predicate action =
+    mkComponentT $ constConditionalT predicate actionT
+    where
+        actionT :: IdentityT Bot ()
+        actionT = lift action
+
+-- | Like conditionalT except the action performed takes no arguments.
+constConditionalT :: BotMonad b
+            -- | The predicate that determines if the specified action is
+            -- allowed to run.
+            =>  (String -> Bool)
+            -- | The action to be executed in the event of a match
             ->  b ()
             -- | Resulting Botable method
             ->  String -> b ()
-conditionalT predicate action  =  onPrivMsgT $ flip when action . predicate
+constConditionalT predicate action = conditionalT predicate $ const action
 
